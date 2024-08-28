5b25f1b50c88bcbb3a4dc7006fcb11ba14dea1a2
sinnosuke2701 <ehgml1111@naver.com>
2024-08-28 17:23:05 +0900

    restaurant

---
+++ himawari/src/main/java/com/shinnosuke/infra/restaurant/RestaurantController.java
@@ -0,0 +1,31 @@
+package com.shinnosuke.infra.restaurant;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RestaurantController {
+	
+	@Autowired
+	RestaurantService RestaurantService;
+	
+	@RequestMapping(value = "/xdm/v1/infra/restaurant/RestaurantXdm")
+	public String RestaurantXdm() {
+		List<RestaurantDto> restaurants = RestaurantService.selectList();
+		
+		for(RestaurantDto restaurantdto : restaurants) {
+			System.out.println(restaurantdto.getSeq() + " | " + restaurantdto.getRes_Type() + " | " + restaurantdto.getRes_Name() + " | " + 
+								restaurantdto.getRes_Open() + " | " + restaurantdto.getRes_Close() + " | " + restaurantdto.getRes_Tel() + " | " + 
+								restaurantdto.getRes_Address() + " | " + restaurantdto.getRes_Parking() + " | " + restaurantdto.getRes_Reservation() + " | " + 
+								restaurantdto.getRes_Corkage() + " | " + restaurantdto.getRes_Introduction() + " | " + restaurantdto.getRes_ReDate() + " | " + 
+								restaurantdto.getRes_MoDate() + " | " + restaurantdto.getRes_DelNY());
+		}
+		
+		
+		return "/xdm/v1/infra/restaurant/RestaurantXdm";
+	}
+	
+}
---
+++ himawari/src/main/java/com/shinnosuke/infra/restaurant/RestaurantDao.java
@@ -0,0 +1,12 @@
+package com.shinnosuke.infra.restaurant;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantDao {
+	
+	List<RestaurantDto> selectList();
+	
+}
