320d1595fd12cc36d990d337e8585894d189d9f9
sinnosuke2701 <ehgml1111@naver.com>
2024-08-29 17:10:34 +0900

    restaurant

--- himawari/src/main/java/com/shinnosuke/infra/restaurant/RestaurantController.java
+++ himawari/src/main/java/com/shinnosuke/infra/restaurant/RestaurantController.java
@@ -4,28 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RestaurantController {
-	
+
 	@Autowired
 	RestaurantService RestaurantService;
-	
+
 	@RequestMapping(value = "/xdm/v1/infra/restaurant/RestaurantXdm")
-	public String RestaurantXdm() {
+	public String RestaurantXdm(Model model) {
 		List<RestaurantDto> restaurants = RestaurantService.selectList();
-		
-		for(RestaurantDto restaurantdto : restaurants) {
-			System.out.println(restaurantdto.getSeq() + " | " + restaurantdto.getRes_Type() + " | " + restaurantdto.getRes_Name() + " | " + 
-								restaurantdto.getRes_Open() + " | " + restaurantdto.getRes_Close() + " | " + restaurantdto.getRes_Tel() + " | " + 
-								restaurantdto.getRes_Address() + " | " + restaurantdto.getRes_Parking() + " | " + restaurantdto.getRes_Reservation() + " | " + 
-								restaurantdto.getRes_Corkage() + " | " + restaurantdto.getRes_Introduction() + " | " + restaurantdto.getRes_ReDate() + " | " + 
-								restaurantdto.getRes_MoDate() + " | " + restaurantdto.getRes_DelNY());
-		}
+
+		model.addAttribute("list4", restaurants);
 		
 		
+//		for (RestaurantDto restaurantdto : restaurants) {
+//			System.out.println(restaurantdto.getSeq() + " | " + restaurantdto.getRes_Type() + " | "
+//					+ restaurantdto.getRes_Name() + " | " + restaurantdto.getRes_Open() + " | "
+//					+ restaurantdto.getRes_Close() + " | " + restaurantdto.getRes_Tel() + " | "
+//					+ restaurantdto.getRes_Address() + " | " + restaurantdto.getRes_Parking() + " | "
+//					+ restaurantdto.getRes_Reservation() + " | " + restaurantdto.getRes_Corkage() + " | "
+//					+ restaurantdto.getRes_Introduction() + " | " + restaurantdto.getRes_ReDate() + " | "
+//					+ restaurantdto.getRes_MoDate() + " | " + restaurantdto.getRes_DelNY());
+//		}
+
 		return "/xdm/v1/infra/restaurant/RestaurantXdm";
 	}
-	
+
 }
